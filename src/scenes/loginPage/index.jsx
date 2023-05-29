612b0fc746b4640a0687facf0bd5b67187d81b25
malekkh <malekkhoder98@gmail.com>
2023-05-29 11:48:46 +0300

    frontend

---
+++ src/scenes/loginPage/index.jsx
@@ -0,0 +1,41 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="left"
+      >
+        <Typography
+          fontWeight="bold"
+          fontSize="32px"
+          fontFamily="cursive"
+          color="#00D5FA"
+        >
+          Memories
+        </Typography>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Memories, the Social Media for Photographers and their
+          memories!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
